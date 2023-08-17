20da6db25e78a82f6f9aecd70f4f799a2d249600
Shubham <shubhammeher109@gmail.com>
2023-08-17 15:12:25 +0530

    Initializing...

---
+++ src/app/home/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Particles from "./particles";
+
+const navigation = [
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+];
+
+export default function Home() {
+  return (
+    <div className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+
+      <div className="absolute left-1/2 top-1/2  h-[500px] w-4/5 md:w-3/5 -translate-x-1/2 -translate-y-1/2 opacity-10 blur-[100px] bg-red-500"></div>
+
+      <div
+        className="flex flex-col item-center text-center md:flex-row gap-0 md:gap-10 leading-none font-display font-bold text-[4rem] md:text-[8rem] md:leading-loose whitespace-nowrap 
+        bg-gradient-to-br from-40% bg-clip-text text-transparent from-red-500 to-white"
+      >
+        <span>Shubham</span> <span>Mehar</span>
+      </div>
+
+      <div className="w-4/5 md:w-2/5 mt-7 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 leading-7">
+        Namaste, wanderer of the web! Step into my domain where the binary heartbeats of technology resonate. I'm <span className="text-blue-500" > Shubham Mehar</span>, and this is my playground
+        </h2>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/navbar/page.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const navigation = ["Portfolio", "Experience", "Education", "Contact"];
+
+const Navbar = () => {
+  return (
+      <ul className="rounded-full inline-flex flex-row gap-8 text-gray-300 text-sm bg-gray-400 px-16 py-4 backdrop-filter backdrop-blur-sm bg-opacity-10">
+       { navigation.map((link) =>
+           (
+             <li className="hover:text-blue-400 hover:-translate-y-0.5 duration-200">
+              <Link href={""}>
+                {link}
+              </Link>
+             </li>
+          )
+         )
+       }
+      </ul>
+  );
+};
+
+export default Navbar;
